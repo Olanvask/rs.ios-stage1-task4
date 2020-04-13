c054daa484fb2ed31a0f64799cc0117928cf0676
Oleg Vasiluk <olegvasiluk@MacBook-Pro-Oleg.local>
2020-04-13 14:05:49 +0300

    feat: exercise 2 compleeted

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -1,8 +1,36 @@
 #import "SquareDecomposer.h"
 
 @implementation SquareDecomposer
+NSMutableArray *tempArray;
+- (NSMutableArray *) decompose: (NSInteger) n with: (NSInteger) member{
+    if (member == 0){
+        NSMutableArray *tempArray = [[NSMutableArray alloc] init];
+        [tempArray addObject: @(n)];
+        return tempArray;
+    }
+
+    for (long i = n - 1; i>0; i--) {
+        if ((member - i*i) >= 0) {
+            tempArray = [self decompose:i with:member - i*i];
+            if (tempArray != nil){
+                [tempArray addObject: @(n)];
+                return tempArray;
+            }
+        }
+    }
+    return nil;
+}
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
     // good luck
-    return @[@0];
+    NSMutableArray *resultArray = [NSMutableArray new];
+    
+    NSInteger n = [number intValue];
+   resultArray =  [[self decompose:n with:n*n] mutableCopy];
+    if (resultArray != nil){
+        [resultArray removeLastObject];
+        return resultArray;
+    }
+    return nil;
+ //   return @[@0];
 }
 @end
